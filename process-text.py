af220ee584a0ec4abff63f133b2ae14bf4cb0d15
gittymutt <pedroschmitz@hotmail.com>
2024-08-26 12:29:14 -0400

    Edit word list. Process text now put words into a list.

--- process-text.py
+++ process-text.py
@@ -1,1 +1,8 @@
-print("hoe")
+import re   
+infile = open('word-list.txt', 'r')
+Lines = infile.readlines()
+
+word_list = []
+for line in Lines:
+    word_list.append(re.split(" ", line.strip())[0])
+print(word_list)
