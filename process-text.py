d65e4dfdc91a7f2f088ee0978291939c8502824e
gittymutt <pedroschmitz@hotmail.com>
2024-08-26 12:33:57 -0400

    process-text.py writes to file.

--- process-text.py
+++ process-text.py
@@ -5,4 +5,9 @@ Lines = infile.readlines()
 word_list = []
 for line in Lines:
     word_list.append(re.split(" ", line.strip())[0])
-print(word_list)
+# print(word_list)
+
+json = "wordList = "
+outfile = open('wordlist-json.txt', 'w')
+outfile.writelines(json)
+outfile.close()
