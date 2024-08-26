3d30cb95e08b36cea156a4eb9eb87b738e0bd433
gittymutt <pedroschmitz@hotmail.com>
2024-08-25 21:00:40 -0400

    FIrst commit

---
+++ index.js
@@ -0,0 +1,126 @@
+let word = "benediction"
+let newWordIndex = 0;
+function sayGuess() {
+  const userAnswer = document.querySelector("#answer-box").value.trim()
+  const rightAnswer = document.querySelector("#answer")
+  const word = rightAnswer.textContent 
+  if (userAnswer == word) {
+    alert("You win!!")
+  } else {
+    alert("You lose. the word is " + word)
+  }
+}
+
+const answer = document.querySelector("#answer")
+const container = document.querySelector("#container")
+const guessButton = document.querySelector("#answer-button")
+// guessButton.addEventListener("click", restartGame )
+
+const newGameButton = document.querySelector("#new-game-button")
+newGameButton.addEventListener("click", () => {
+  const newWord = wordList[newWordIndex]
+  newWordIndex++
+  setBoard(newWord)
+})
+
+function setCharAt(str,index,chr) {
+    if(index > str.length-1) return str;
+    return str.substring(0,index) + chr + str.substring(index+1);
+}
+
+function putBar(angle) {
+  const container = document.querySelector("#container")
+  let el = document.createElement("div")
+  el.classList.add("bar")
+  el.style.cssText += `rotate: ${angle}deg`
+  container.appendChild(el)
+}
+
+function putLetter(letter, angle) {
+  const container = document.querySelector("#container")
+  let el = document.createElement("p")
+  let span = document.createElement("span")
+  let text = document.createTextNode(letter)
+  el.appendChild(span)
+  span.appendChild(text)
+  span.classList.add("block-span")
+  el.style.cssText += `transform: rotate(${angle}deg);`
+  span.style.cssText += `transform: rotate(-${angle}deg);`
+  container.appendChild(el)
+}
+
+function setBoard(word) {
+  document.querySelector("#answer-box").focus()
+  const answer = document.querySelector("#answer")
+  answer.textContent = word
+  console.log(answer.textContent)
+  guessButton.removeEventListener("click", sayGuess)
+  guessButton.addEventListener("click", sayGuess )
+  // alert("word is now " + word)
+  let letterCount = word.length
+  container.innerHTML = ''
+  // shift the letters
+  let randShift = Math.floor(Math.random() * letterCount);
+  wordArray = word.split('')
+  wordArray = wordArray.concat(wordArray.splice(0,randShift))
+  word = wordArray.join('')
+    console.log(word)
+
+  // maybe reverse the letters
+  let doReverse = Math.floor(Math.random() * 2);
+  if (doReverse) {
+    word = [...word].reverse().join("")
+  }
+
+
+  // replace a random letter with a question mark
+  let randSlot = Math.floor(Math.random() * letterCount);
+  word = setCharAt(word, randSlot, "?")
+
+  let degrees = 360 / letterCount;
+  let starter = degrees / 2
+  console.log(word)
+  let letter = word[0]
+  letter = letter.slice(1)
+  for (let angle  = 0 + starter; angle < 360 + starter+1; angle = angle + degrees) {
+    putLetter(letter, angle)
+    letter = word[0]
+    word = word.slice(1)
+    putBar(angle-starter)
+  }
+
+}
+
+
+setBoard(word)
+
+const wordList = [
+  "communist",
+  "liberal",
+  "fascist",
+  "obedience"
+] 
+
+
+
+
+
+// let span = document.createElement("span")
+// let text = document.createTextNode("1")
+// el.appendChild(span)
+// span.appendChild(text)
+// span.classList.add("block-span")
+// el.style.cssText += "transform: rotate(90deg); "
+// span.style.cssText += "transform: rotate(-90deg);"
+// container.appendChild(el)
+
+
+// el = document.createElement("p")
+// span = document.createElement("span")
+// text = document.createTextNode("2")
+// el.appendChild(span)
+// span.appendChild(text)
+// span.classList.add("block-span")
+// el.style.cssText += "transform: rotate(190deg); "
+// span.style.cssText += "transform: rotate(-190deg);"
+// container.appendChild(el)
